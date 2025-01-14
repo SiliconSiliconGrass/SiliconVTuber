336777a4aefd2b74eef48673aae179d9f4ee2b21
硅硅草 <14764340+silicon-grass@user.noreply.gitee.com>
2025-01-14 14:15:44 +0800

    detach tokens and model_dir from source code

--- src/components/MaoDemo.vue
+++ src/components/MaoDemo.vue
@@ -11,7 +11,7 @@
             <canvas id="live2d-canvas" class="live2d-canvas">您的浏览器不支持canvas!</canvas>
         </div>
 
-        <div class="visualize-area">
+        <div v-if="debug" class="visualize-area">
             <!-- 行为数据可视化区域 -->
             <div v-if="actionQueueWatcher" class="action-queue">
                 <div v-for="(action, i) in actionQueueWatcher" :key="i" class="action-container">
@@ -50,8 +50,7 @@
 import AudioBank from './ResourceManager/AudioBank.vue';
 import Mao from './BotBrain/MaoCore.vue';
 import ResourceManager from './ResourceManager/ResourceManager.vue';
-
-import { Resource } from './ResourceManager/ResourceManager.vue';
+import axios from 'axios';
 
 export default {
     components: {
@@ -59,10 +58,12 @@ export default {
     },
     data() {
         return {
+            debug: false,
 
-            // PAT: 'pat_bGvnxxW2UYNwE2UL2pReUuNIZF0JMIK9OtP0BdUh5gzw5yHECLohwM8MxsoeLF10', // Personal access token
-            PAT: 'pat_JEvKfnxhGwlEi510PQTwADBA4hJIJqQMlrgE373qdlDJrRyGrExskzAdsZkxz6Dw',
-            PAT2: 'pat_JEvKfnxhGwlEi510PQTwADBA4hJIJqQMlrgE373qdlDJrRyGrExskzAdsZkxz6Dw', // Using two different tokens. Maybe unnecessary
+            // Using two tokens. Maybe unnecessary
+            PAT: '', // bot brain token
+            PAT2: '', // tts bot token
+
             MAO_BOT_ID: '7444170557848977471', // Mao
             TTS_BOT_ID: '7444603592826159141', // Mao Speaker
             USER_ID: 'some_user_id', // The user ID
@@ -79,12 +80,22 @@ export default {
     },
 
     methods: {
+        async getToken() {
+            try {
+                const response = await axios.get('/CozeToken/token.txt', {
+                responseType: 'text' // 确保响应类型是文本
+                });
+                console.log('got token:', response.data);
+                this.PAT = response.data;
+                this.PAT2 = this.PAT;
+            } catch (error) {
+                console.error('Error fetching the text file:', error);
+            }
+        },
+
         enableAudioActivities() {
             // enableAudio();
             this.$refs.mao_audio_bank.handleUserGesture();
-        },
-        l2dInit() {
-            //
         },
 
         sendChat(message) {
@@ -93,42 +104,20 @@ export default {
     },
 
     mounted() {
-        this.l2dInit();
 
-        // loadOml2d({
-        //     models: [
-        //         // 在这里进行配置
-        //         // {
-        //         //     path: 'https://model.oml2d.com/Senko_Normals/senko.model3.json',
-        //         //     position: [-10, 20]
-        //         // },
-        //         {
-        //             path: "/Models/Mao/Mao.model3.json",
-        //             position: [10, 20]
-        //         }
-        //     ]
-        // });
+        this.getToken()
+        .then(() => {
+            this.Mao = new Mao(
+                this.PAT, this.MAO_BOT_ID, this.USER_ID,
+                this.PAT2, this.TTS_BOT_ID,
+            );
 
-        this.Mao = new Mao(
-            this.PAT, this.MAO_BOT_ID, this.USER_ID,
-            this.PAT2, this.TTS_BOT_ID,
-            //
-        );
+            this.actionQueue = this.Mao.actionQueue;
 
-        this.actionQueue = this.Mao.actionQueue;
-
-        var resourceManager = new ResourceManager(this.Mao, this.$refs.mao_audio_bank);
-        this.Mao.resourceManager = resourceManager;
-        this.resourceManager = resourceManager;
-
-        this.$refs.input_area.addEventListener("keydown", (event) => {
-            if (event.key === 'Enter') {
-                this.sendChat(this.inputText);
-                this.inputText = '';
-            }
+            var resourceManager = new ResourceManager(this.Mao, this.$refs.mao_audio_bank);
+            this.Mao.resourceManager = resourceManager;
+            this.resourceManager = resourceManager;
         });
-
-        // this.Mao.respondTo("你好呀! 今天过的怎么样? 想我了吗?"); // Test
 
         setInterval(() => {
             // 可视化
@@ -142,67 +131,19 @@ export default {
             this.resourcesWatcher = [];
             if (this.resourceManager) {
                 for (let id of this.resourceManager.resourceIds) {
-                    this.resourcesWatcher.push(resourceManager.get(id));
+                    this.resourcesWatcher.push(this.resourceManager.get(id));
                 }
             }
         }, 200);
 
-
-        /* 口播 */
-
-        
-
-        const TEXT = `
-谢伊·吉尔杰斯-亚历山大将在周一雷霆队对阵华盛顿奇才队的比赛中，为自己的首个MVP奖项增添砝码。这位效力七年的后卫在周六晚上的比赛中再次砍下30分，帮助雷霆在其创队纪录的15连胜被终结后重振旗鼓。此番，亚历山大仅在29分钟内就砍下全场最高的39分，助力雷霆以126-101大胜主队纽约尼克斯。亚历山大21投15中，雷霆一度领先多达30分。这位两届全明星球员本赛季已22次砍下30+，领跑全联盟，场均贡献31.5分、5.9次助攻、5.5个篮板和2.0次抢断。
-在战胜纽约尼克斯的比赛中，亚历山大得到了大量的帮助。伊赛亚·乔三分球11投8中，替补出场砍下31分，而杰伦·威廉姆斯则贡献了19分、4个篮板和5次助攻。雷霆队在对阵华盛顿奇才队的最近五次交锋中全部获胜，而奇才队目前以6胜30负的战绩位列NBA垫底。
-奇才队在周六的客场比赛中以105-138不敌芝加哥公牛队，遭遇五连败，公牛队全场投篮命中率达到55.8%，三分球命中率更是高达46.9%。“防守转换、控制内线、争抢投篮、篮板球、额外努力……所有这些方面我们做得都不够好，”奇才队主教练布莱恩·基夫告诉《华盛顿邮报》。“我们没有在细节和专注度上达到必要的水平，无法阻止他们。这是我的责任。我们必须做得更好。”
-奇才队后卫马尔科姆·布罗格登因右脚疼痛已缺席最近三场比赛，能否出战周日的比赛仍存疑问。布罗格登的缺席为后备后卫贾里德·巴特勒提供了机会，他在过去四场比赛中场均得到18.8分和5.3次助攻。
-奇才队正在围绕一群年轻球员进行重建，其中包括后卫凯肖恩·乔治。虽然这位21岁的新秀投篮命中率仅为33.2%，三分球命中率更是只有25.3%，但基夫仍然对他充满信心。“我一点也不担心这个，他的投篮会回来的。他是个不错的投手。”
-        `;
-
-        // const TEXT = `这是一段测试文本！可以`;
-
-        function multipleSplit(inputString, delimiters) {
-            // 构建一个正则表达式，匹配任一指定的分隔符
-            const delimiterRegex = new RegExp('[' + delimiters.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&') + ']+');
-            // 使用正则表达式分割字符串
-            return inputString.split(delimiterRegex);
-        }
-
-        const seps = "。？！?!\n";
-        var splitList = multipleSplit(TEXT, seps);
-        var self = this.Mao;
-        for (var i = 0; i < splitList.length; i++) {
-            // 处理一个短句
-            let sentence = splitList[i];
-
-            // 处理方括号[]中的motion/expression信息
-            let sentenceSplit = sentence.split('[');
-
-            for (let split of sentenceSplit) {
-                let splitSplit = split.split(']');
-                if (splitSplit.length == 1) {
-                    if (splitSplit[0] !== '') {
-                        let resource = new Resource(self.uuid(), 'TTS', {text: splitSplit[0]});
-                        self.resourceManager.add(resource); // 注册所需TTS audio 资源
-                        self.actionQueue.enqueue({type: "SayAloud", data: splitList[i], resources: [resource]}); // 将SayAloud动作加入队列
-                    }
-                } else {
-                    // splitSplit[0]: expression/motion name
-                    // splitSplit[1]: tts text
-
-                    // expression/motion 应该不需要resource
-                    this.actionQueue.enqueue({type: "Expression/Motion", data: splitSplit[0], resources: []}); // Expression/Motion动作入队
-                    
-                    if (splitSplit[1] !== '') {
-                        let resource = new Resource(self.uuid(), 'TTS', {text: splitSplit[1]});
-                        self.resourceManager.add(resource); // 注册所需TTS audio 资源
-                        self.actionQueue.enqueue({type: "SayAloud", data: splitList[i], resources: [resource]}); // 将SayAloud动作加入队列
-                    }
-                }
+        this.$refs.input_area.addEventListener("keydown", (event) => {
+            if (event.key === 'Enter') {
+                this.sendChat(this.inputText);
+                this.inputText = '';
             }
-        }
+        });
 
+        // this.Mao.respondTo("你好呀! 今天过的怎么样? 想我了吗?"); // Test
     },
 };
 </script>
